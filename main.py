043a7904c9c72f6d1fea30f316c0fbbb5a98d3e0
kritika0598 <3319-kritika.singhal@users.noreply.source.golabs.io>
2021-10-20 11:18:29 +0530

    style: use pep8 style conventions

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ excel_data_df = pandas.read_excel('records.xlsx')
 json_str = excel_data_df.to_json(orient='records')
 json_str = json.loads(json_str)
 
-amount = {"extra": 0}
+amount = {'extra': 0}
 for key, value in conversion_dict.items():
     amount[value] = 0
 
 for data in json_str:
-    debit_amount = data["Debit"]
+    debit_amount = data['Debit']
     if isinstance(debit_amount, str):
         debit_amount = debit_amount.replace(',', '')
-    credit_amount = data["Credit"]
+    credit_amount = data['Credit']
     if isinstance(credit_amount, str):
         credit_amount = float(credit_amount.replace(',', ''))
     if credit_amount is None:
@@ -26,7 +26,7 @@ for data in json_str:
         debit_amount = 0
     assigned = False
     for key, value in conversion_dict.items():
-        if key.lower() in data["Description"].lower():
+        if key.lower() in data['Description'].lower():
             amount[value] = amount[value] - debit_amount + credit_amount
             assigned = True
             break
